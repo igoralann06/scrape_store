bc76f77368d2d3e10cca11fb525bacb51a634d78
Igor <igoralann113@gmail.com>
2024-09-27 22:05:57 +0900

    added user-agent

--- main.py
+++ main.py
@@ -16,7 +16,10 @@ def clean_filename(filename):
 
 def scrape_store(url):
     # Send a GET request to the URL
-    response = requests.get(url)
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
+    }
+    response = requests.get(url, headers=headers)
 
     # Parse the HTML content of the page
     soup = BeautifulSoup(response.content, 'html.parser')
--- shell.py
+++ shell.py
@@ -51,8 +51,10 @@ store_urls = [
 
 for url in store_urls:
     
-    response = requests.get(url)
-    print(response)
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
+    }
+    response = requests.get(url, headers=headers)
 
     # Parse the HTML content of the page
     soup = BeautifulSoup(response.content, 'html.parser')
