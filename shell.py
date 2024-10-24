01868cfa2bfbfe70780b86da30299702f7cb520e
Igor <igoralann113@gmail.com>
2024-10-25 02:11:47 +0900

    updated

--- shell.py
+++ shell.py
@@ -48,6 +48,7 @@ for url in store_urls:
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
         }
         response = requests.get(url, headers=headers)
+        print(response)
 
         # Parse the HTML content of the page
         soup = BeautifulSoup(response.content, 'html.parser')
