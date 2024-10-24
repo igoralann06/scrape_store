406cb35773f24271f2e189920ed5c9f1e285d259
Igor <igoralann113@gmail.com>
2024-10-25 02:10:05 +0900

    updated

--- shell.py
+++ shell.py
@@ -42,8 +42,8 @@ os.mkdir("resources/"+current_time)
 section_id = 1
 
 for url in store_urls:
+    print(url)
     try:
-    
         headers = {
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
         }
